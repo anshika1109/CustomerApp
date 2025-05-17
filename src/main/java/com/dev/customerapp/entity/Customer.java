a667b44e71d728bd06e5f4a6ff195e802a2edb79
anshikagupta <anshika1109@gmail.com>
2025-05-16 19:59:07 -0500

    Implemented customer app-v1

--- src/main/java/com/dev/customerapp/entity/Customer.java
+++ src/main/java/com/dev/customerapp/entity/Customer.java
@@ -1,6 +1,7 @@
 package com.dev.customerapp.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
@@ -13,6 +14,7 @@ public class Customer {
     private long id;
 
     @NotBlank(message = "Name is mandatory")
+    @Valid
     private String name;
 
     @NotBlank(message = "Email is mandatory")
